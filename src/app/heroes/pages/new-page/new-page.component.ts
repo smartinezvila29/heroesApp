4e2b213c6576bf39b0d82353846eb15f20c8a043
smartinezvila29 <smartinezvila29@gmail.com>
2024-03-06 22:09:57 +0100

    first reactive form

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/heroe.interface';
+import { Heroe, Publisher } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styleUrl: './new-page.component.css'
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
@@ -24,8 +28,54 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ]
 
-  onSubmit() {
-    console.log({formIsValid: this.heroForm.valid,
-      value: this.heroForm.value})
+  constructor( private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private snackbar: MatSnackBar){}
+
+
+  ngOnInit(): void {
+    if( !this.router.url.includes('edit')){ return; }
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroeById(id) )
+    ).subscribe( hero =>
+      {
+        if ( !hero ) return this.router.navigateByUrl('/');
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
+
+
+  get currentHero(): Heroe{
+    const hero = this.heroForm.value as Heroe;
+    return hero;
+  }
+
+  onSubmit(): void{
+    if(this.heroForm.invalid){
+      return;
     }
+
+    if( this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe( heroe => this.showSnackbar('Updated'));
+        return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe( heroe => {
+        this.router.navigate(['/heroes/edit', heroe.id]);
+        this.showSnackbar('Created');
+      });
+
+  }
+
+  showSnackbar(message: string): void{
+    this.snackbar.open(message, 'done', {
+      duration: 2500
+    });
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
 import { environments } from '../../../environments/environments';
 
@@ -25,6 +25,25 @@ export class HeroesService {
 
   getSuggestion(query: string): Observable<Heroe[]>{
     return this.httpClient.get<Heroe[]>(this.baseUrl + '/heroes?q=' + query +'&_limit=6');
+  }
+
+  addHero( hero: Heroe ): Observable<Heroe> {
+    return this.httpClient.post<Heroe>(`${ this.baseUrl }/heroes`, hero );
+  }
+
+  updateHero( hero: Heroe ): Observable<Heroe> {
+    if ( !hero.id ) throw Error('Hero id is required');
+
+    return this.httpClient.patch<Heroe>(`${ this.baseUrl }/heroes/${ hero.id }`, hero );
+  }
+
+  deleteHeroById( id: string ): Observable<boolean> {
+
+    return this.httpClient.delete(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        map( resp => true ),
+        catchError( err => of(false) ),
+      );
   }
 
 }
